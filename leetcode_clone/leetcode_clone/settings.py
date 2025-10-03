1c7678d8e22f4e4392b644ac085cca575f32e42c
Abhi Jain <abhijainaj2003@gmail.com>
2025-10-03 23:46:08 +0530

    changed Allowed Host

--- leetcode_clone/leetcode_clone/settings.py
+++ leetcode_clone/leetcode_clone/settings.py
@@ -37,7 +37,7 @@ RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
     
-ALLOWED_HOSTS.append('leetcode-clone-frontend.onrender.com') 
+ALLOWED_HOSTS.append('https://leetcode-clone-frontend.onrender.com/') 
 
 
 
