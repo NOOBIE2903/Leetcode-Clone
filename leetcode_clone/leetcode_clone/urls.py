a22738098952409a014a8f5a92b08ccd35471767
Abhi Jain <abhijainaj2003@gmail.com>
2025-10-04 00:09:32 +0530

    updated the urls file

--- leetcode_clone/leetcode_clone/urls.py
+++ leetcode_clone/leetcode_clone/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -8,10 +10,21 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('users/', include('users.urls')),
-    path('problems/', include('problems.urls')),
-    path('submissions/', include('submissions.urls')),
-    path('contests/', include('contests.urls')),
+    path('api/', include([
+        path('users/', include('users.urls')),
+        path('problems/', include('problems.urls')),
+        path('submissions/', include('submissions.urls')),
+        path('contests/', include('contests.urls')),
+        path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+        path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    ])),
+    # path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('users/', include('users.urls')),
+    # path('problems/', include('problems.urls')),
+    # path('submissions/', include('submissions.urls')),
+    # path('contests/', include('contests.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
