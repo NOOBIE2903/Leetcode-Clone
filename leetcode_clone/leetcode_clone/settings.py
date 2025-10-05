082a30646672eefa4926fc21c9f773bb0f1a5be2
Abhi Jain <abhijainaj2003@gmail.com>
2025-10-05 14:20:55 +0530

    changed the settings.py file

--- leetcode_clone/leetcode_clone/settings.py
+++ leetcode_clone/leetcode_clone/settings.py
@@ -15,6 +15,7 @@ from datetime import timedelta
 import os 
 from dotenv import load_dotenv 
 import dj_database_url
+from django.core.management.utils import get_random_secret_key
 load_dotenv()
 
 
@@ -26,12 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
